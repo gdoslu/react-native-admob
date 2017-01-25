6cd8a37f5d66bce1ed7b67c2ba104608b42538d9
Gokhan Doslu <gkhndsl@gmail.com>
2017-01-25 16:22:49 +0300

    Fixed issue about upgrading React 0.40

--- ios/RNAdMobInterstitial.h
+++ ios/RNAdMobInterstitial.h
@@ -1,7 +1,6 @@
-#import "RCTBridgeModule.h"
-#import "RCTEventDispatcher.h"
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventDispatcher.h>
 @import GoogleMobileAds;
 
 @interface RNAdMobInterstitial : NSObject <RCTBridgeModule, GADInterstitialDelegate>
 @end
-
